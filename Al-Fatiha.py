235f9d7f54ca0184a9180bae8b7ccc7766b39eee
Mafia-King5 <116032756+Mafia-King5@users.noreply.github.com>
2022-10-30 17:35:29 +0600

    Add files via upload

---
+++ Al-Fatiha.py
@@ -0,0 +1,152 @@
+#Tools Created By All Hacking Tools
+#AUTHOR : All Hacking Tools
+#Tools : Al-Fatiha
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+ 
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+ 
+os.system('clear')
+print
+ 
+print
+
+psb('\x1b[1;32mBismillahir Rahmaner Rahim.')
+ 
+print("""
+
+\033[0;93m
+\033[0;93m /$$$$$$  /$$$$$$  /$$        /$$$$$$  /$$      /$$
+\033[0;93m|_  $$_/ /$$__  $$| $$       /$$__  $$| $$$    /$$$
+\033[0;93m  | $$  | $$  \__/| $$      | $$  \ $$| $$$$  /$$$$
+\033[0;93m  | $$  |  $$$$$$ | $$      | $$$$$$$$| $$ $$/$$ $$
+\033[0;93m  | $$   \____  $$| $$      | $$__  $$| $$  $$$| $$
+\033[0;93m  | $$   /$$  \ $$| $$      | $$  | $$| $$\  $ | $$
+\033[0;93m /$$$$$$|  $$$$$$/| $$$$$$$$| $$  | $$| $$ \/  | $$
+\033[0;93m|______/ \______/ |________/|__/  |__/|__/     |__/
+\033[0;93m
+
+""")
+
+print("\033[1;96mThis Tool's Free By All Hacking Tools")
+
+psb('\x1b[1;92mSURA AL-FATIHA START PLEASE WAIT.....')
+ 
+def exb():
+    print
+    os.sys.exit()
+ 
+ 
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+ 
+ 
+def t():
+    time.sleep(1)
+ 
+ 
+def cb():
+    os.system('clear')
+    
+logo = """                                                                     
+\033[0;94m \\\\\\\\\\\\\\\\\\\\\WELCOME TO AHT////////////////
+\033[0;96m=============================================
+\033[0;92m            _        ______    _   _ _           
+\033[0;92m      /\   | |      |  ____|  | | (_) |          
+\033[0;92m     /  \  | |______| |__ __ _| |_ _| |__   __ _ 
+\033[0;92m    / /\ \ | |______|  __/ _` | __| | '_ \ / _` |
+\033[0;92m   / ____ \| |      | | | (_| | |_| | | | | (_| |
+\033[0;92m  /_/    \_\_|      |_|  \__,_|\__|_|_| |_|\__,_|
+\033[0;92m
+\033[0;96m =============================================
+\033[0;93m AUTHOR       : All Hacking Tools
+\033[0;94m PAGE         : All Hacking Tools
+\033[0;95m YOUTUBE      : All Hacking Tools
+\033[0;96m GITHUB       : AHT1-AHT1
+\033[0;96m =============================================                                           
+"""
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+ 
+def menu():
+    os.system('clear')
+    print logo
+    print ''
+    print '\033[0;92mSURA AL-FATIHA'
+    print '\033[0;92m'
+    print 50* '-'
+    print '\033[0;92m[1]  \x1b[1;92mAl-Fatiha'
+    print '\033[0;91m[0]  \x1b[1;91mExit'
+    print '\033[0;92m'
+    print 50 * '-'
+    action()
+    
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\033[0;92m\n> ')
+    if bch == '':
+        print
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print 50* '-'
+        print ''
+        print 'Al-Fatiha'
+        print''
+        try:
+            c = raw_input('\033[0;92mChoose Code : ')
+            
+ 
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '0':
+        exb()
+    else:
+        print
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] Bismillahir Rahmanir Rohim...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x96] Alhamdulillahi Rabbil Alamin...')
+    time.sleep(1.9)
+    psb('Arrahmanir Rahim...')
+    time.sleep(1.9)
+    psb('Maliki Eyawmiddin...')
+    time.sleep(1.9)
+    psb('Eya Kana Budu Eya Kanastain...')
+    time.sleep(1.9)
+    psb('Ehdinas Siratal Mustakim...')
+    time.sleep(1.9)
+    psb('Siratallazina Anamta Alaihim Gairil Magdubi Alaihim Waladdin...')
+    time.sleep(1.9)
+    psb('Amin...')
+    time.sleep(1.9)
+    psb(50 * '-')
+    time.sleep(0.5)
+    print
+    50 * '-'
+    print
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 Al-Fatiha.py')
+ 
+ 
+if __name__ == '__main__':
+    menu()
